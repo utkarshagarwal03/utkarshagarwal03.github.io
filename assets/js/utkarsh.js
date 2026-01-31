f7b5e694a27bfc3a63e28ea4691eb0c6c8b148fd
Utkarsh Agarwal <162401935+utkarshagarwal03@users.noreply.github.com>
2026-01-31 10:19:05 +0530

    Refactor navigation link event listenerst
    Refactor navigation link event handling for clarity and efficiency.

--- assets/js/utkarsh.js
+++ assets/js/utkarsh.js
@@ -136,24 +136,18 @@ for (let i = 0; i < formInputs.length; i++) {
 
 
 
-// page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    const targetPage = link.getAttribute("data-nav-link");
 
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
-    }
+    pages.forEach(page => {
+      page.classList.toggle("active", page.dataset.page === targetPage);
+    });
 
+    navLinks.forEach(btn => btn.classList.remove("active"));
+    link.classList.add("active");
   });
-}
+});
